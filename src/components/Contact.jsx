a5998d6d31fb2c71dca1efd98665d56e4fc3463d
Kaique Devesa <kaiquedeveza1@hotmail.com>
2025-02-07 21:22:00 -0300

    fazendo a validacao dos inputs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,17 +2,11 @@ import React, { useState } from "react";
 import "./Contact_style.css";
 
 export const Contact = () => {
-  const [selectedOption, setSelectedOption] = useState("");
-
-  const handleClick = (value) => {
-    setSelectedOption(value);
-  };
-
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
     email: "",
-    queryType: "",
+    query: null,
     message: "",
     consent: false,
   });
@@ -25,26 +19,102 @@ export const Contact = () => {
       ...formData,
       [name]: type === "checkbox" ? checked : value,
     });
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: "",
+    }));
+  };
+
+  const validateForm = () => {
+    let newErrors = {};
+
+    if (!formData.firstName || formData.firstName.trim() === "") {
+      newErrors.firstName = "First Name is required";
+    }
+
+    if (!formData.lastName || formData.lastName.trim() === "") {
+      newErrors.lastName = "Last Name is required";
+    }
+
+    if (!formData.email || formData.email.trim() === "") {
+      newErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Invalid email format";
+    }
+
+    if (!formData.query || formData.query.trim() === "") {
+      newErrors.query = "Query Type is required";
+    }
+
+    if (!formData.message || formData.message.trim() === "") {
+      newErrors.message = "Message is required";
+    }
+
+    if (!formData.consent) {
+      newErrors.consent = "You must accept the terms";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      alert("Form submitted sucessfully!", formData);
+
+      setFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        query: "",
+        message: "",
+        consent: false,
+      });
+
+      setErrors({});
+    }
   };
 
   return (
-    <div className="container-contact">
+    <form className="container-contact" onSubmit={handleSubmit}>
       <h1 className="title">Contact Us</h1>
 
       <div className="form-name">
         <div className="first-name">
           <p>First Name *</p>
-          <input type="text" />
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+          {errors.firstName && (
+            <span className="error">{errors.firstName}</span>
+          )}
         </div>
         <div className="last-name">
           <p>Last Name *</p>
-          <input type="text" />
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+          {errors.lastName && <span className="error">{errors.lastName}</span>}
         </div>
       </div>
 
       <div className="email">
         <p>Email Adress * </p>
-        <input type="text" />
+        <input
+          type="text"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        {errors.email && <span className="error">{errors.email}</span>}
       </div>
 
       <div className="query-type">
@@ -52,38 +122,37 @@ export const Contact = () => {
         <div className="options">
           <div
             className={`query-option ${
-              selectedOption === "general" ? "option-click" : ""
+              formData.query === "general" ? "option-click" : ""
             }`}
-            onClick={() => handleClick("general")}
           >
             <input
               type="radio"
               id="check-g"
               name="query"
               value="general"
-              checked={selectedOption === "general"}
-              onChange={() => handleClick("general")}
+              checked={formData.query === "general"}
+              onChange={handleChange}
             />
             <label htmlFor="check-g">General Enquiry</label>
           </div>
 
           <div
             className={`query-option ${
-              selectedOption === "suport" ? "option-click" : ""
+              formData.query === "suport" ? "option-click" : ""
             }`}
-            onClick={() => handleClick("suport")}
           >
             <input
               type="radio"
               id="check-s"
               name="query"
               value="suport"
-              checked={selectedOption === "suport"}
-              onChange={() => handleClick("suport")}
+              checked={formData.query === "suport"}
+              onChange={handleChange}
             />
             <label htmlFor="check-s">Support Request</label>
           </div>
         </div>
+        {errors.query && <span className="error">{errors.query}</span>}
       </div>
 
       <div>
@@ -92,20 +161,29 @@ export const Contact = () => {
           className="text-message"
           name="message"
           id="message"
+          value={formData.message}
+          onChange={handleChange}
         ></textarea>
+        {errors.message && <span className="error">{errors.message}</span>}
       </div>
 
       <div className="box-confirm">
-        <input type="checkbox" id="confirm" />
+        <input
+          type="checkbox"
+          id="confirm"
+          name="consent"
+          value={formData.consent}
+        />
         <label htmlFor="confirm">
           I consent to being contacted by the team *
         </label>
+        {errors.consent && <span className="error">{errors.message}</span>}
       </div>
 
       <div className="container-submit">
         <button className="submit">Enviar</button>
       </div>
-    </div>
+    </form>
   );
 };
 
